6ee33c625d72e6b8e34a46a9c4ec905d571f6ed5
James Wu <jameswu144@gmail.com>
2024-07-24 15:19:08 +0100

    add fisher-yates shuffle

--- Utility.cs
+++ Utility.cs
@@ -42,4 +42,15 @@ public static class Utility
             Console.WriteLine();
         }
     }
+
+    // Fisher-Yates Shuffle
+    public static void Shuffle<T>(T[] array)
+    {
+        Random random = new();
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = random.Next(i + 1);
+            (array[j], array[i]) = (array[i], array[j]);
+        }
+    }
 }
