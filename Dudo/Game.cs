4139f78b0fc7c94eed201c45450a848b68336f3f
James Wu <jameswu144@gmail.com>
2024-07-24 21:18:39 +0100

    terminal state

--- Dudo/Game.cs
+++ Dudo/Game.cs
@@ -10,6 +10,8 @@ public class Game
     public int[] claimNum;
     public int[] claimRank;
 
+    public readonly Random random;
+
     public Game(int sides = 6)
     {
         NumSides = sides;
@@ -29,8 +31,68 @@ public class Game
         {
             for (int j = 0; j < NumSides; j++)
             {
-                claimRank[i * NumSides + j] = (j + 1) % NumSides + 1;
+                claimRank[i * NumSides + j] = (j + 1) % NumSides; // for now dice are from 0 to 5
             }
         }
+
+        random = new();
     }
+
+    public double CFR(bool[] isClaimed, double p1, double p2)
+    {
+        int plays = 0;
+        foreach (bool claim in isClaimed)
+        {
+            if (claim) plays++;
+        }
+
+        int player = plays % 2;
+        int opponent = 1 - player;
+
+
+        // Check if history is terminal
+        if (isClaimed[^1])
+        {
+            // player 'player' is dudo
+
+            // simulate dice rolls
+            int[] rollCounts = new int[NumSides];
+            rollCounts[random.Next(NumSides)]++;
+            rollCounts[random.Next(NumSides)]++;
+
+            // Verify the claims
+            int multiplier = player == 0 ? 1 : -1;
+
+            // If claim is true, we lose
+            return VerifyClaim(isClaimed, rollCounts) ? -multiplier : multiplier;
+        }
+
+
+
+
+
+        return 0.0;
+    }
+
+    private int InfoSetToInteger(int roll, bool[] isClaimed)
+    {
+        int res = roll;
+        for (int a = NumActions - 2; a >= 0; a--)
+        {
+            res = res * 2 + (isClaimed[a] ? 1 : 0);
+        }
+
+        return res;
+    }
+
+    private bool VerifyClaim(bool[] isClaimed, int[] rollCounts)
+    {
+        for (int a = 0; a < NumActions - 1; a++)
+        {
+            if (!isClaimed[a]) continue;
+            if (rollCounts[claimRank[a]] < claimNum[a]) return false;
+        }
+        return true;
+    }
+
 }
--- Dudo/Node.cs
+++ Dudo/Node.cs
@@ -1,7 +1,77 @@
 
 namespace CFR.Dudo;
 
+// This is basically the same as in Kuhn Poker so look to merge the two after this
 public class Node
 {
+    public int NumActions;
+    public int infoID;
+    public double[] regretSum;
+    public double[] strategy;
+    public double[] strategySum;
 
+    public Node(int NumActions, int infoID)
+    {
+        this.NumActions = NumActions;
+        this.infoID = infoID;
+
+        regretSum   = new double[NumActions];
+        strategy    = new double[NumActions];
+        strategySum = new double[NumActions];
+    }
+
+    public double[] GetStrategy(double realisationWeight)
+    {
+        double normalizingSum = 0;
+        for (int a = 0; a < NumActions; a++)
+        {
+            strategy[a] = regretSum[a] > 0 ? regretSum[a] : 0;
+            normalizingSum += strategy[a];
+        }
+
+        for (int a = 0; a < NumActions; a++)
+        {
+            if (normalizingSum > 0)
+            {
+                strategy[a] /= normalizingSum;
+            }
+            else
+            {
+                strategy[a] = 1.0 / NumActions;
+            }
+
+            strategySum[a] += realisationWeight * strategy[a];
+        }
+
+        return strategy;
+    }
+
+    public double[] GetAverageStrategy()
+    {
+        double[] avgStrategy = new double[NumActions];
+        double normalizingSum = 0;
+        for (int a = 0; a < NumActions; a++)
+        {
+            normalizingSum += strategySum[a];
+        }
+
+        for (int a = 0; a < NumActions; a++)
+        {
+            if (normalizingSum > 0)
+            {
+                avgStrategy[a] = strategySum[a] / normalizingSum;
+            }
+            else
+            {
+                avgStrategy[a] = 1.0 / NumActions;
+            }
+        }
+
+        return avgStrategy;
+    }
+
+    public override string ToString()
+    {
+        throw new NotImplementedException();
+    }
 }
